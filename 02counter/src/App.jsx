16b86f0fc6b3f13c6fb947656c1e923fbcf7473d
Ganeshg1313 <ganeshcoder333@gmail.com>
2024-01-21 12:53:32 +0530

    updated

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  // let counter = 15;
+
+  let [counter, setCounter] = useState(0);
+
+  const increaseValue = () => {
+    if(counter != 20){
+      setCounter(++counter);
+    }
+  }
+
+  const decreaseValue = ()=>{
+    if(counter != 0){
+      setCounter(--counter);
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur code</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <button onClick={increaseValue}>Increase Value {counter}</button>
+      <br/>
+      <br/>
+      <button onClick={decreaseValue}>Decrease Value {counter}</button>
+      <br />
+      <br />
+      footer : {counter}
+    </>
+  )
+}
+
+export default App
